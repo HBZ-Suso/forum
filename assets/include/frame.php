8b419109634082546fd84613ba5bd8e21056bf0b
Lucifer <lucifermonao@gmx.de>
2020-11-18 11:28:49 +0100

    Improved frame, added menu

--- assets/include/frame.php
+++ assets/include/frame.php
@@ -1,12 +1,25 @@
 <?php 
 
 if ($info->mobile === true) {
+    echo '
+    <link rel="stylesheet" href="/forum/assets/style/mobile.frame.css">
+    <div class="main-heading-container">
+        <img class="main-menu-icon" src="https://img.icons8.com/material-rounded/50/000000/menu.png"/>
 
+        <h1 class="main-heading-text">' . $text->get("frame-mobile-heading") . '</h1>
+    </div>
+    ';
 } else {
+    if (isset($_SESSION["user"])) {
+        $account = $text->get("frame-menu-account");
+    } else {
+        $account = $text->get("frame-menu-login");
+    }
+
     echo '
     <link rel="stylesheet" href="/forum/assets/style/pc.frame.css">
     <div class="main-heading-container">
-        <img class="main-menu-icon" src="https://img.icons8.com/material-rounded/50/000000/menu.png"/>
+        <img class="main-menu-icon" src="https://img.icons8.com/material-rounded/1000/000000/menu.png"/>
 
         <h1 class="main-heading-text">' . $text->get("frame-pc-heading") . '</h1>
 
@@ -15,13 +28,23 @@ if ($info->mobile === true) {
             <input type="submit" class="main-heading-search-submit" value="->">
         </form>
     </div>
+    <div class="main-menu">
+        <a class="main-menu-entry" href="/forum/?show=account">' . $account . '</a><br>
+        <a class="main-menu-entry" href="/forum/?show=about">' . $text->get("frame-menu-about") . '</a><br>
+    </div>
+
+    <script>
+        var hidden = true;
+        document.querySelector(".main-menu-icon").addEventListener("click", (e) => {
+            if (hidden === true) {
+                document.querySelector(".main-menu").style.display = "initial";
+                hidden = false;
+            } else {
+                document.querySelector(".main-menu").style.display = "none";
+                hidden = true;
+            }
+        })
+    </script>
     ';
-
-
-
-
-
-
-
 }
 
