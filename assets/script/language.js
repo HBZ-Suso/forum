236fd28bddbf5f6ff0a6905c95cdec81bb405c39
Lucifer <lucifermonao@gmx.de>
2021-01-26 10:27:37 +0100

    Fixed Pop-ups showing undefined instead of options

--- assets/script/language.js
+++ assets/script/language.js
@@ -1,24 +1,34 @@
 function set_language () {
     get_text("language-switcher-question").then(function (result) {
-        window.prompt(result).then(() => {
-            var language_prompt_answer = document.getElementById("q-input").value;
+        axios
+            .post("/forum/assets/api/get_languages.php")
+            .then((languages) => {
+                if (languages.data.length > 1) {
+                    var language_str = languages.data.join(", ");
+                } else {
+                    var language_str = languages.data.join("");
+                }
+                window.prompt(result, language_str).then(() => {
+                var language_prompt_answer = document.getElementById("q-input").value;
 
-            if (language_prompt_answer.length > 0) {
-                axios
-                .post("/forum/assets/api/language.php?language=" + language_prompt_answer)
-                .then((response) => {
-                    if (response.data.indexOf("error") === -1) {
-                        window.location.reload();
-                    }
-                })
-                .catch((error) => {
-                    throw new Error(error);
-                })
-            }
-        }).catch(() => {
-            console.debug("Prompt error");
+                if (language_prompt_answer.length > 0) {
+                    axios
+                    .post("/forum/assets/api/language.php?language=" + language_prompt_answer)
+                    .then((response) => {
+                        if (response.data.indexOf("error") === -1) {
+                            window.location.reload();
+                        }
+                    })
+                    .catch((error) => {
+                        throw new Error(error);
+                    })
+                }
+            }).catch(() => {
+                console.debug("Prompt error");
+            })
         })
-    })
+        .catch((e) => {throw new Error(e)});
+    });
 }
 
 if (document.getElementById("language-switcher") !== null) {
--- assets/script/theme.js
+++ assets/script/theme.js
@@ -1,24 +1,36 @@
 function set_theme () {
     get_text("theme-switcher-question").then(function (result) {
-        window.prompt(result).then(() => {
-            var theme_prompt_answer = document.getElementById("q-input").value;
-
-            if (theme_prompt_answer !== "") {
-                axios
-                .post("/forum/assets/api/theme.php", "theme=" + theme_prompt_answer)
-                .then((response) => {
-                    if (response.data === theme_prompt_answer) {
-                        document.getElementById("theme-box").innerHTML = "";
-                        document.getElementById("theme-box").innerHTML = '<link rel="stylesheet" href="/forum/assets/theme/' + response.data + '.css">';
+        axios
+            .post("/forum/assets/api/get_themes.php")
+            .then((themes) => {
+                if (themes.data.length > 1) {
+                    var theme_str = themes.data.join(", ");
+                } else {
+                    var theme_str = themes.data.join("");
+                }
+                window.prompt(result, theme_str).then(() => {
+                    var theme_prompt_answer = document.getElementById("q-input").value;
+        
+                    if (theme_prompt_answer !== "") {
+                        axios
+                        .post("/forum/assets/api/theme.php", "theme=" + theme_prompt_answer)
+                        .then((response) => {
+                            if (response.data === theme_prompt_answer) {
+                                document.getElementById("theme-box").innerHTML = "";
+                                document.getElementById("theme-box").innerHTML = '<link rel="stylesheet" href="/forum/assets/theme/' + response.data + '.css">';
+                            }
+                        })
+                        .catch((error) => {
+                            throw new Error(error);
+                        })
                     }
+                }).catch(() => {
+                    console.debug("Prompt error");
                 })
-                .catch((error) => {
-                    throw new Error(error);
-                })
-            }
-        }).catch(() => {
-            console.debug("Prompt error");
-        })
+            })
+            .catch((error) => {
+                throw new Error(error);
+            })
     })
 }
 
